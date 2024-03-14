1186d02779e16c16e89ded292db5db3bfbf0434e
Jose-Salazar-30 <jsalazarm10@miumg.edu.gt>
2024-03-13 20:31:54 -0600

    Agregar Clase Persona

---
+++ Persona.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+class Persona {
+	
+	protected : string nombres, apellidos, direccion, fecha_nacimiento;
+				int telefono;
+				
+	
+	protected:
+		Persona(){
+		}
+		
+		Persona(string nom, string ape, string dir, string fn, int tel) {
+			nombres = nom;
+			apellidos = ape;
+			direccion = dir;
+			fecha_nacimiento = fn;
+			telefono = tel;
+		}
+		
+	void crear();
+	void leer();
+	void actualizar();
+	void borrar();
+};
